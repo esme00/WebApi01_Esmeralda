fca0d755d41e592ba97477be8b6d6c6061cfb066
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-02-17 08:55:36 -0600

    Agregar archivos de proyecto.

---
+++ WebApi01_Esmeralda/Controllers/equiposController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using WebApi01_Esmeralda.Models;
+
+namespace WebApi01_Esmeralda.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto;    
+
+        public equiposController(equiposContext equiposContexto) 
+        {
+            _equiposContexto = equiposContexto;
+        }
+
+        ///sumary
+        /// EndPoint que retorna el listado de todos los equipos existentes
+        /// summary
+        /// <return></return>
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos
+                                           select e).ToList();
+
+            if (listadoEquipo.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoEquipo);
+        }
+    }
+
+  
+  
+
+   
+}
